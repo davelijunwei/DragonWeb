09a6d2212c7cd9a4b352ab7bf3c9afc118d5ff83
davelijunwei <lijunwei.dave@gmail.com>
2021-09-23 13:52:29 +0800

    commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css'
+import RobotController from './pages/RobotController'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+  return <RobotController />
+}
+
+export default App
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+//Setup LoginPage for Admin
+
+// import React from 'react'
+
+// function LoginPage() {
+//   return <div></div>
+// }
+
+// export default LoginPage
